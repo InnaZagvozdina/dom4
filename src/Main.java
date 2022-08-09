e970834147c8d8a78d8e525e7de30942b44e3f58
Inna <inna-asbest@mail.ru>
2022-08-09 21:13:07 +0500

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,34 @@ public class Main {
         for (int a = 10; a > 0; a--) {
             System.out.print(a + " ");
 
-           //
+            //
         }
-           // Задание 2
+        // Задание 2
 
-            int friday = 1;
-            for (; i < 31; i = i +7) {
-                friday = friday + 7;
-               System.out.println();
-                System.out.println("Сегодня пятница,  день "  +friday +"  необходимо подготовить отчет");
+        int friday = 1;
+        for (; i < 31; i = i + 7) {
+            friday = friday + 7;
+            System.out.println();
+            System.out.println("Сегодня пятница,  день " + friday + "  необходимо подготовить отчет");
+            System.out.println();
+        }
 
-                // Задание 3
+        // Задание 3
 
-
-
+        int start = 1822;
+        for (int year = 1822; year <= 2122; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
 
 
             }
         }
     }
+}
+
+
+
+
 
 
 
