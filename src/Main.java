756976f65f81aa93e799872150a3c774563b7340
Inna <inna-asbest@mail.ru>
2022-08-09 20:08:03 +0500

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.security.PublicKey;
 public class Main {
     public static void main(String[] args) {
 
-          // Задание 1
+        // Задание 1
         int i = 0;
         while (i < 10) {
             i++;
@@ -13,14 +13,21 @@ public class Main {
         for (int a = 10; a > 0; a--) {
             System.out.print(a + " ");
 
-            // Задание 2
+           //
+        }
+           // Задание 2
+
+            int friday = 1;
+            for (; i < 31; i = i +7) {
+                friday = friday + 7;
+               System.out.println();
+                System.out.println("Сегодня пятница,  день "  +friday +"  необходимо подготовить отчет");
 
 
-
-
+            }
         }
     }
-}
+
 
 
 
