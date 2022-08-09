5c857a565398ec5e258f5c1841deeae3762a5901
Inna <inna-asbest@mail.ru>
2022-08-09 18:54:44 +0500

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.security.PublicKey;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Всем привет");
+
+          // Задание 1
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int a = 10; a > 0; a--) {
+            System.out.print(a + " ");
+
+            // Задание 2
+
+
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
